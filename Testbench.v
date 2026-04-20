a20de61a34deeb035932a7cff3d2edc62f168b97
Ritav <ritav08092004@gmail.com>
2026-04-20 14:20:58 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.04.2026 09:20:43
-// Design Name: 
-// Module Name: tb_LUT
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_LUT();
 reg [5:0]a_ti; 
