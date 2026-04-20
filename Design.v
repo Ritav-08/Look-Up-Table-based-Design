b8adfe4807dabbc12a5cb9fa0bf4d64c9652ca41
Ritav <ritav08092004@gmail.com>
2026-04-20 14:20:44 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.04.2026 09:20:14
-// Design Name: 
-// Module Name: LUT
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module LUT(
    input a_i,
